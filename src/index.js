593053fab1860567933088c7acc5d29d68733d92
Branislav Bugarcic <bugarcic@talon.one>
2019-09-24 00:38:01 +0100

    parse function for parsing nested validation messages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 export function parse(messages) {
-    return messages;
+    const keys = Object.keys(messages);
+    const values = keys.map(key => messages[key]);
+    let result = values.reduce((acc, message, index) => {
+        if (typeof message === 'string') {
+            acc = `${acc}<li>${messages[keys[index]]}</li>`;
+        }
+        if (typeof message === 'object') {
+            acc = `${acc}<li>${parse(message)}</li>`;
+        }
+        return acc;
+    }, '<ul>');
+    result = `${result}</ul>`;
+    return result;
 }
