c0cb8030a3bc5eb13dc77e462dad2d1893f99fad
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-19 16:10:17 +0100

    Tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 export function parse(messages) {
-    return messages;
+    if(typeof messages === 'string') return messages
+
+    if(typeof messages === 'object') {
+        const children = Object.values(messages)
+        const lists = children.reduce(
+            (lists, child) => lists += `<li>${parse(child)}</li>`,
+            ''
+        )
+        return `<ul>${lists}</ul>`
+    }
 }
