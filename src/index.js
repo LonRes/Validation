4369d48133de810520be7f6cc85cad207317ab8b
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-19 16:14:33 +0100

    Naive error checking

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 export function parse(messages) {
-    if(typeof messages === 'string') return messages
-
-    if(typeof messages === 'object') {
-        const children = Object.values(messages)
-        const lists = children.reduce(
-            (lists, child) => lists += `<li>${parse(child)}</li>`,
-            ''
-        )
-        return `<ul>${lists}</ul>`
+    switch(typeof messages) {
+        case 'string':
+            return messages
+        case 'object': {
+            const children = Object.values(messages)
+            const lists = children.reduce(
+                (lists, child) => lists += `<li>${parse(child)}</li>`,
+                ''
+            )
+            return `<ul>${lists}</ul>`
+        }
+        default:
+            return ''
     }
 }
