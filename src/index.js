1534ddef7aa2b765ec357163f29044b819170641
Gagandeep Singh <gagandeep.singh@nhsbt.nhs.uk>
2022-02-02 18:19:46 +0000

    Created a new recursive function to loop thru the messages object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 export function parse(messages) {
-    return messages;
+    return createList(messages);
+}
+
+function createList(messages, existingList = '') {
+    existingList = existingList + '<ul>';
+    for (const prop in messages) {
+        existingList = existingList + '<li>';
+        if (typeof messages[prop] === 'object') {
+            existingList = createList(messages[prop], existingList);
+        } else {
+            existingList = existingList + messages[prop];
+        }
+        existingList = existingList + '</li>';
+    }
+    existingList = existingList + '</ul>';
+    return existingList;
 }
